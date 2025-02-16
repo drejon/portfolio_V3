085854ddc1b4a51e391169a152c9ee62ce1c637a
Diego Rejón <drc.working@gmail.com>
2025-02-16 21:54:15 +0100

    Install Clipboard

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [
+    // CommonModule,
+    ClipboardModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  
+  copy(): string {
+    return 'drc.working@gmail.com'
+  }
 }
