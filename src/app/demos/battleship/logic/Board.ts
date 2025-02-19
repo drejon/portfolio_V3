5a5dc7c988417bb307aaed55ce4d80d1032de5f3
Diego <drc.working@gmail.com>
2025-02-19 15:38:08 +0100

    Start Battleship demo

---
+++ src/app/demos/battleship/logic/Board.ts
@@ -0,0 +1,12 @@
+export class Board {
+  private board;
+  constructor() {
+    this.board = this.createBoard();
+  }
+
+  private createBoard() {
+    const newBoard = [];
+
+    for (const y of Array(10)) {return}
+  }
+}
--- src/app/demos/battleship/test/unitest/Game.test.js
+++
@@ -1,9 +0,0 @@
-// import { expect } from 'chai';
-// import { Game } from '../../logic/Game';
-describe('Game', function() {
-  context('initialises game', function() {
-    it('creates the game board', function() {
-      expect(true).toBe(true)
-    })
-  })
-})
